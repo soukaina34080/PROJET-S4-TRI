702dddcd863fb2c7e268766d2d345a9a2abe5728
soukaina34080 <45172668+soukaina34080@users.noreply.github.com>
2019-02-25 10:13:52 +0100

    Update tribase.py

--- PycharmProjects/PROJET-TRI/tribase.py
+++ PycharmProjects/PROJET-TRI/tribase.py
@@ -2,44 +2,45 @@
 import random
 import time
 
-
-def radixSort(nbElem):
+ 
+def radixSort(nbElem): #on définit une fonction qui prend en paramètre nbElem
     start_time = time.time()
-    list_vals = random.sample(range(1000),nbElem)
-    RADIX = 10
-    maxLength = False
-    tmp = -1
-    placement = 1
+    list_vals = random.sample(range(1000),nbElem) #on créé une liste de nbElem éléments de valeurs aléatoires inférieures ou égale à 1000
+    maxLength = False  #On initialise maxLenght à False qui est vrai si on est sur le dernier placement 
+    tmp = -1            #On initialise tmp à -1 
+    placement = 1      #On initialise placement à 1 qui indique le placement dans notre chiffre
 
-    while not maxLength:
+    
+    while not maxLength: 
         maxLength = True
-        buckets = [list() for _ in range( RADIX )]
-
-        for i in list_vals:
-            tmp = i // placement
-            buckets[tmp % RADIX].append( i )
-            if maxLength and tmp > 0:
-                maxLength = False
+        buckets = [list() for _ in range( 10 )] # On créé un tableau de 10 tableaux
+        # Parcours list_val et saisie les valeurs dans les buckets
+        for i in list_vals:                     
+            tmp = int(i / placement)
+            buckets[tmp % 10].append(i) 
+            if maxLength and tmp > 0:  
+                maxLength = False            
 
         a = 0
-
-        for b in range( RADIX ):
+        #Parcours buckets et saisie les valeurs dans list_val
+        for b in range( 10 ):
             buck = buckets[b]
+
             for i in buck:
                 list_vals[a] = i
                 a += 1
-
-        placement *= RADIX
+    
+        placement *= 10
     print(list_vals);
-    tempsEc = time.time() - start_time;
+tempsEc = time.time() - start_time;
     print("Temps d'execution : %s secondes" % (tempsEc))
     
     
     f = open( '%dtribase.txt' %nbElem, 'a' )
     f.write( str(tempsEc) + '\n' )
     f.close() 
-     
-
+        
+    
 
 
 def main():
