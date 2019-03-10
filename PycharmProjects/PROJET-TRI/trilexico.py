8ce5ab22c32b6dbddf0f03a02f5eeeb8962a0201
lailachm <45227648+lailachm@users.noreply.github.com>
2019-03-10 18:57:44 +0100

    Update trilexico.py

--- PycharmProjects/PROJET-TRI/trilexico.py
+++ PycharmProjects/PROJET-TRI/trilexico.py
@@ -5,43 +5,39 @@ import time
 from string import ascii_letters
 
 def sortLexo(nbElem): 
-    start_time = time.time() #enregistre le temps de début
-    listwords = ["".join(random.choice(ascii_letters) 
-                         for j in range(random.randint(2,10)) ) #nb lettre mot
-                 for i in range(nbElem) ] #nb mot
+    start_time = time.time() # enregistre le temps de départ
+    listwords = ["".join(random.choice(ascii_letters) # choisi une lettre au hasard (le join va permettre de créer un mot(joindre les lettres))
+                         for j in range(random.randint(2,10)) ) # nombre de lettres dans le mot (choisi au hasard)
+                 for i in range(nbElem) ] # nombre de mots dans la liste à trier
   
-    # tri chaine de carac
+    # tri les mots de la liste dans l'odre lexicographique
     listwords.sort() 
-
-    # affiche les mots dans l'ordre alphabetique
-    # for i in listwords: 
-    #    print( i )  
-        
     
-    print(listwords) #affiche la liste de mots triés
+    # affiche la liste de mots triés
+    print(listwords) 
     
-    tempsEc = time.time() - start_time; #temps écoulé
-    print("Temps d'execution : %s secondes" % (tempsEc))
+    tempsEc = time.time() - start_time; # calcul du temps écoulé (temps actuel moins le temps de départ)
+    print("Temps d'execution : %s secondes" % (tempsEc)) # affiche à l'écran le temps écoulé
     
-    
-    f = open( '%dtrilexico.txt' %nbElem, 'a' ) #ouvre le fichier dans lequel on va enregistrer le temps
+    # ouvre le fichier dans lequel on va enregistrer le temps qui va être nommé après le nombre d'éléments dans la liste
+    f = open( '%dtrilexico.txt' %nbElem, 'a' ) 
     f.write( str(tempsEc) + '\n' )
     f.close() 
     
-    #calcul de la moyenne du temps d'exécution de l'algo
+    # calcul de la moyenne du temps d'exécution de l'algo
     somme = 0
     moyenne = 0
     nbLigne = 0
-    cr = csv.reader(open("%dtrilexico.csv" %nbElem, "r"))
+    cr = csv.reader(open("%dtrilexico.csv" %nbElem, "r")) # on ouvre le fichier où sont enregistrer les temps
     for r in cr: #r = colonne
-        somme  += float(r[0])
-        nbLigne += 1
+        somme  += float(r[0]) # on somme chacun des temps
+        nbLigne += 1 # on compte le nombre de lignes (= nombre de temps enregistrés)
     print("Somme temps : %s" % somme)
-    moyenne = somme / nbLigne
-    print("Moyenne : %s" % moyenne)
-    print("Nb valeur : %s " % nbLigne)
+    moyenne = somme / nbLigne # calcul du temps moyen
+    print("Moyenne : %s" % moyenne) # affiche le temps moyen
+  
     
-    #stockage du temps moyen dans un fichier
+    # stockage du temps moyen dans un fichier
     moy = open('%dmoytrilexico.csv' % nbElem, 'w')
     moy.write(str(moyenne) + '\n')
     moy.close()
@@ -51,4 +47,4 @@ if __name__ == '__main__':
 
 
     
-    sortLexo(10)
+    sortLexo(10) # appel de l'algorithme
